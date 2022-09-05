94525cc3e93178e3159392c81131ef742d3c9c3c
WFaustin <wf77@drexel.edu>
2022-09-05 19:56:07 -0400

    Remade branch for Svelte

---
+++ CommunityTiered/src/routes/+page.svelte
@@ -0,0 +1,70 @@
+<script>
+	import Counter from '$lib/Counter.svelte';
+
+	$: count = 1
+	function handleClick(){
+		count +=1; 
+	}
+	$: doubled = 2 ** count; 
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		<span class="welcome">
+			<picture>
+				<source srcset="svelte-welcome.webp" type="image/webp" />
+				<img src="svelte-welcome.png" alt="Welcome" />
+			</picture>
+		</span>
+		<main>
+			<slot />
+		</main>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
+
+<div id='content'>
+	<p on:click={handleClick}> Counter for exponent click is at {count} {count === 1 ? 'time' : 'times'}. Exponent is {doubled}</p>
+</div>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
